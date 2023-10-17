303f27469c4b3902b508287a36b0699fc1b5daed
shubham-guru <gurushubham4@gmail.com>
2023-10-17 22:20:08 +0530

    Generate id add

--- src/presentation/components/Licence.tsx
+++ src/presentation/components/Licence.tsx
@@ -1,13 +1,72 @@
-import { Row, Col } from 'antd'
-import React from 'react'
+import { Row, Col, Modal, Form, Input, Button } from 'antd'
+import React, { useState } from 'react'
+import { UserOutlined } from "@ant-design/icons";
+import "../css/licence.css";
 import Header from './hocs/Header'
 
 const Licence = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [email, setEmail] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const onFinish = () => {
+
+  }
+
   return (
-    <Row className=''>
+    <Row>
         <Col className='header-col' span={23}>
-            <Header heading='Licence' />
+            <Header heading='Licence' getCLick={showModal} />
         </Col>
+
+        <Modal title="Generate Unique Id" centered open={isModalOpen} footer={null} onOk={handleOk} onCancel={handleCancel}>
+        <Form
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+            className='generate-id-email'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="generate-form-button"
+            loading={loading}
+          >
+            Generate Unique ID
+          </Button>
+        </Form.Item>
+        </Form>
+      </Modal>
     </Row>
   )
 }
--- src/presentation/components/hocs/Header.tsx
+++ src/presentation/components/hocs/Header.tsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import { Col, Input, Row, Typography, Image } from 'antd'
-import { SearchProps } from 'antd/es/input'
-import {BellOutlined} from "@ant-design/icons"
-import "../../css/heading.css"
+import React from "react";
+import { Col, Input, Row, Typography, Image, Button } from "antd";
+import { SearchProps } from "antd/es/input";
+import { BellOutlined, PlusOutlined } from "@ant-design/icons";
+import "../../css/heading.css";
 
 type IHeading = {
-    heading: string,
-}
-const Header:React.FC<IHeading> = ({heading}) => {
-    const { Text } = Typography
-    const { Search } = Input;
-    const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+  heading: string;
+  getCLick?: () => void;
+};
+const Header: React.FC<IHeading> = ({ heading, getCLick }) => {
+  const { Text } = Typography;
+  const { Search } = Input;
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
+    console.log(info?.source, value);
   return (
-    <Row className='header-row'>
-        <Col span={16}>
-            <Text className='heading'>{heading}</Text>
-        </Col>
+    <Row className="header-row" gutter={10}>
+      <Col span={heading === "Licence" || heading === "Properties" ? 10 : 16}>
+        <Text className="heading">{heading}</Text>
+      </Col>
+      <Col span={6}>
+        <Search placeholder={`Search ${heading}`} onSearch={onSearch} />
+      </Col>
+      {heading === "Licence" || heading === "Properties" ? (
         <Col span={6}>
-            <Search placeholder={`Search ${heading}`} onSearch={onSearch} />
+          <Button
+            className="generate-id-btn"
+            icon={<PlusOutlined />}
+            onClick={getCLick}
+          >
+            Generate Unique ID
+          </Button>
         </Col>
-        <Col span={2}>
-            <BellOutlined className='bell-icon' />
-        </Col>
+      ) : null}
+      <Col span={2}>
+        <BellOutlined className="bell-icon" />
+      </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
